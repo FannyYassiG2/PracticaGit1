809181b5e8feccda5e9fe90c9e72fe591ee9a0d6
Fanny <estefania.26yg@gmail.com>
2021-04-07 19:50:52 -0500

    V 2.0 Archivos con estilos y texto

--- index.php
+++ index.php
@@ -4,10 +4,66 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link rel="stylesheet" href="estilos.css">
 	<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
 	<title>Inicio</title>
 </head>
 <body>
-	
+	<p> 
+	Estefania Yassi Gomez
+	</p>
+	<hr class="featurette-divider" id="descripcion">
+
+      <div class="row featurette">
+        <div class="col-md-6">
+          <h2 class="featurette-heading" id="texto1">Mis metas </h2>
+          <p class="lead"  id="texto">-El mayor peligro para la mayoría no reside en establecer una meta demasiado alta y fracasar, sino en establecerla demasiado corta y conseguirla.   	
+      	</p>
+      	<br>
+      	<h4 id="autor">
+          -Michelangelo Buonarroti.
+        </h4>
+
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" src="metas.jpg" alt="Imagen metas">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+          <h2 class="featurette-heading" id="texto1">Mis obstaculos </h2>
+          <p class="lead" id="texto">El miedo puede evitar que avancemos. Si ves el obstáculo como algo que te impide alcanzar tu objetivo entonces así será. En lugar de permitir que el obstáculo dicte tus acciones, piensa en el éxito que puedes lograr si superas ese obstáculo. Toda dificultad se convierte en una experiencia de aprendizaje. </p>
+          <br>
+          <h4 id="autor">
+          	-Anonimo
+          </h4>
+        </div>
+        <div class="col-md-5 col-md-pull-7">
+          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" src="obstaculo.jpg" alt="Imagen obstaculo">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading" id="texto1">Lo mas valioso</h2>
+          <p class="lead"  id="texto">Saber cosas nuevas es el bien más valioso que hay sobre la superficie de la Tierra. Con cuantas más verdades trabajemos, más ricos nos haremos.</p>
+          	<br>
+          <h4 id="autor">
+          	-Kurt Vonnegut
+          </h4>
+          	
+          
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" src="valioso.png" alt="Imagen Valioso">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
 </body>
 </html>
